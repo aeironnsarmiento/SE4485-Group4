3c76bbd93bad9fe28ffde164661c10db224b2bf1
JCGarzaa <jgarza0127@yahoo.com>
2024-04-26 16:13:35 -0500

    feat: User can specify destination path as arg

--- package.py
+++ package.py
@@ -3,21 +3,35 @@ This file packages a VSCode environment from the user's machine into the current
 This will keep the user's extensions, settings, keybindings, and snippets and put them into a single zip file
 This will be executed only on windows machines
 """
-print("Packaging VSCode environment...")
 import os
 import shutil
+import sys
 
-# Copy the user's VSCode settings and extensions to the current directory
-shutil.copytree(os.path.join(os.getenv("APPDATA"), "Code"), "vscode/Code")
+def main():
 
-# copy the user's .vscode directory to the current directory
-shutil.copytree(os.path.join(os.getenv("USERPROFILE"), ".vscode"), "vscode/.vscode")
+    print("Packaging VSCode environment...")
 
-# Zip the copied directory
-shutil.make_archive("vscode", "zip", "vscode")
+    # Copy the user's VSCode settings and extensions to the current directory
+    shutil.copytree(os.path.join(os.getenv("APPDATA"), "Code"), "vscode/Code")
 
-# Remove the copied directory
-shutil.rmtree("vscode")
+    # copy the user's .vscode directory to the current directory
+    shutil.copytree(os.path.join(os.getenv("USERPROFILE"), ".vscode"), "vscode/.vscode")
 
-print("Packaging complete.")
+    # Zip the copied directory
+    if len(sys.argv) == 1:
+        shutil.make_archive("vscode", "zip", "vscode")
+    elif len(sys.argv) == 2:
+        destination_path = sys.argv[1]
+        shutil.make_archive(destination_path + "vscode", "zip", "vscode")
+    else:
+        print("Usage: python package.py [optional: destination_path]")
+        sys.exit(1)
 
+    # Remove the copied directory
+    shutil.rmtree("vscode")
+
+    print("Packaging complete.")
+
+
+if __name__ == "__main__":
+    main()
